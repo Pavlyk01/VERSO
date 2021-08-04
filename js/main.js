341bf19489f66750b1a1189469965d3388f5af90
Pavlyk01 <81038218+Pavlyk01@users.noreply.github.com>
2021-08-04 23:54:22 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,11 +17,3 @@ $(function() {
 
 
 })
-$(function() {
-    var scrollEventHandler = function() {
-        window.scroll(0, window.pageYOffset)
-    }
-
-    window.addEventListener("scroll", scrollEventHandler, false);
-
-})
